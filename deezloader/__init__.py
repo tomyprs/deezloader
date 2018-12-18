b616dcc1010b6f0b7f697b4955f8d21bba77df54
An0nimia <41164945+An0nimia@users.noreply.github.com>
2018-12-18 19:22:00 +0100

    Add files via upload

--- deezloader/__init__.py
+++ deezloader/__init__.py
@@ -14,7 +14,6 @@ from collections import OrderedDict
 from binascii import a2b_hex, b2a_hex
 from mutagen.flac import FLAC, Picture
 from Crypto.Cipher import AES, Blowfish
-req = requests.Session()
 localdir = os.getcwd()
 def generate_token():
     token = oauth2.SpotifyClientCredentials(client_id="4fe3fecfe5334023a1472516cc99d805", client_secret="0f02b7c483c04257984695007a4a8d5c").get_access_token()
@@ -50,6 +49,8 @@ class QualityNotFound(Exception):
           super().__init__(message)
 class Login:
       def __init__(self, mail, password):
+          global req
+          req = requests.Session()
           check = json.loads(req.post("http://www.deezer.com/ajax/gw-light.php", params).text)['results']['checkFormLogin']
           post_data = {
                        "type": "login",
@@ -61,10 +62,9 @@ class Login:
            print("Success, you are in")
           else:
               raise BadCredentials("Invalid password or username")
-      def download(self, track, location, quality, check):
-          song = {}
+      def download(self, track, location, quality, check, extension):
           ids = track.split("/")[-1]
-          name = ids + ".mp3"
+          name = ids + extension
           def login():
               try:
                  token = json.loads(req.post("http://www.deezer.com/ajax/gw-light.php", params).text)['results']['checkForm']
@@ -85,8 +85,8 @@ class Login:
               h = MD5.new()
               h.update(data)
               return b2a_hex(h.digest())
-          def genurl(quality):
-              data = b"\xa4".join(a.encode() for a in [song['md5'], quality, str(ids), str(song['media_version'])])
+          def genurl(md5, quality, media):
+              data = b"\xa4".join(a.encode() for a in [md5, quality, str(ids), str(media)])
               data = b"\xa4".join([md5hex(data), data]) + b"\xa4"
               if len(data) % 16:
                data += b"\x00" * (16 - len(data) % 16)
@@ -112,8 +112,6 @@ class Login:
           infos = login()
           while not "MD5_ORIGIN" in str(infos):
               infos = login()
-          song['md5'] = infos['results']['MD5_ORIGIN']
-          song['media_version'] = infos['results']['MEDIA_VERSION']
           try:
              if int(infos['results']['FILESIZE_' + quality]) > 0 and quality == "FLAC":
               quality = "9"
@@ -128,20 +126,26 @@ class Login:
                   raise QualityNotFound("The quality chose can't be downloaded")
                  quality = "1"
           except KeyError:
-             raise QualityNotFound("The quality chose can't be downloaded")
+             if check == True:
+              raise QualityNotFound("The quality chose can't be downloaded")
+             quality = "1" 
           try:
-             fh = requests.get(genurl(quality))
+             crypt = requests.get(genurl(infos['results']['MD5_ORIGIN'], quality, infos['results']['MEDIA_VERSION']))
           except:
-             fh = requests.get(genurl(quality))
-          if len(fh.content) == 0:
+             crypt = requests.get(genurl(infos['results']['MD5_ORIGIN'], quality, infos['results']['MEDIA_VERSION']))
+          if len(crypt.content) == 0:
            raise TrackNotFound("")
-          open(location + name, "wb").write(fh.content)
-          fo = open(location + name, "wb")
-          decryptfile(fh.iter_content(2048), calcbfkey(ids), fo)
-      def download_trackdee(self, URL, output=localdir + "/Songs/", check=True, quality="MP3_128", recursive=True):
+          open(location + name, "wb").write(crypt.content)
+          decry = open(location + name, "wb")
+          decryptfile(crypt.iter_content(2048), calcbfkey(ids), decry)
+      def download_trackdee(self, URL, output=localdir + "/Songs/", check=True, quality="MP3_320", recursive=True):
           if output == localdir + "/Songs":
            if not os.path.isdir("Songs"):
             os.makedirs("Songs")
+          if quality == "FLAC":
+           extension = ".flac"
+          else:
+              extension = ".mp3"
           array = []
           music = []
           artist = []
@@ -164,7 +168,7 @@ class Login:
           except KeyError:
              None
           try:
-             if "error" in str(url):
+             if url['error']:
               raise InvalidLink("Invalid link ;)")
           except KeyError:
              None
@@ -177,25 +181,21 @@ class Login:
               raise QuotaExceeded("Too much requests limit yourself")
           except KeyError:
              None
-          try:
-             image = url['album']['cover_xl'].replace("1000", "1200")
-          except:
-             try:
-                image = requests.get(URL).text
-             except:
-                image = requests.get(URL).text
-             image = BeautifulSoup(image, "html.parser").find("img", class_="img_main").get("src").replace("120", "1200")
+          image = url['album']['cover_xl'].replace("1000", "1200")
+          if image == "":
+           try:
+              image = requests.get(URL).text
+           except:
+              image = requests.get(URL).text
+           image = BeautifulSoup(image, "html.parser").find("img", class_="img_main").get("src").replace("120", "1200")
           music.append(url['title'])
           for a in url['contributors']:
               array.append(a['name'])
           if len(array) > 1:
            for a in array:
-               for b in range(len(array)):
-                   try:
-                      if a in array[b] and a != array[b]:
-                       del array[b]
-                   except IndexError:
-                      break
+               for b in array:
+                   if a in b and a != b:
+                    array.remove(b)
           artist.append(", ".join(OrderedDict.fromkeys(array)))
           album.append(url['album']['title'])
           tracknum.append(url['track_position'])
@@ -210,26 +210,24 @@ class Login:
           try:
              for a in url1['genres']['data']:
                  genre.append(a['name'])
-          except:
+          except KeyError:
              None
-          for a in url1['contributors']:
-              if a['role'] == "Main":
-               ar_album.append(a['name'])
+          ar_album.append(url['artist']['name'])
           dir = str(output) + "/" + artist[0].replace("/", "").replace("$", "S") + "/"
           try:
              os.makedirs(dir)
-          except:
+          except FileExistsError:
              None
-          name = artist[0].replace("/", "").replace("$", "S") + " " + music[0].replace("/", "").replace("$", "S") + ".mp3"
+          name = artist[0].replace("/", "").replace("$", "S") + " " + music[0].replace("/", "").replace("$", "S") + extension
           if os.path.isfile(dir + name):
            if check == False:
             return dir + name
            ans = input("Song already exist do you want to redownload it?(y or n):")
-           if not ans == "y":
+           if ans != "y":
             return
           print("\nDownloading:" + song)
           try:
-             self.download(URL, dir, quality, recursive)
+             self.download(URL, dir, quality, recursive, extension)
           except TrackNotFound:
              try:
                 url = json.loads(requests.get("https://api.deezer.com/search/track/?q=" + music[0].replace("#", "") + " + " + artist[0].replace("#", "")).text)
@@ -262,15 +260,11 @@ class Login:
                         break
                 except IndexError:
                    raise TrackNotFound("Track not found: " + song)
-             self.download(URL, dir, quality, recursive)
+             self.download(URL, dir, quality, recursive, extension)
           try:
-             os.rename(dir + URL.split("/")[-1] + ".mp3" , dir + name)
+             os.rename(dir + URL.split("/")[-1] + extension , dir + name)
           except FileNotFoundError:
              None
-          try:
-             image = requests.get(image).content
-          except:
-             image = requests.get(image).content
           try:
              tag = EasyID3(dir + name)
              tag.delete()
@@ -303,10 +297,14 @@ class Login:
           except:
              None
           return dir + name
-      def download_albumdee(self, URL, output=localdir + "/Songs/", check=True, quality="MP3_128", recursive=True):
+      def download_albumdee(self, URL, output=localdir + "/Songs/", check=True, quality="MP3_320", recursive=True):
           if output == localdir + "/Songs":
            if not os.path.isdir("Songs"):
             os.makedirs("Songs")
+          if quality == "FLAC":
+           extension = ".flac"
+          else:
+              extension = ".mp3"
           array = []
           music = []
           artist = []
@@ -331,23 +329,21 @@ class Login:
           except KeyError:
              None
           try:
-             if "error" in str(url):
+             if url['error']:
               raise InvalidLink("Invalid link ;)")
           except KeyError:
              None
-          try:
-             image = url['cover_xl'].replace("1000", "1200")
-          except:
-             try:
-                image = requests.get(URL).text
-             except:
-                image = requests.get(URL).text
-             image = BeautifulSoup(image, "html.parser").find("img", class_="img_main").get("src").replace("200", "1200")
-          for a in url['tracks']['data']:
-              music.append(a['title'])
-              urls.append(a['link'])
+          image = url['cover_xl'].replace("1000", "1200")
+          if image == "":
+           try:
+              image = requests.get(URL).text
+           except:
+              image = requests.get(URL).text
+           image = BeautifulSoup(image, "html.parser").find("img", class_="img_main").get("src").replace("200", "1200")
           for a in url['tracks']['data']:
               del array[:]
+              music.append(a['title'])
+              urls.append(a['link'])
               try:
                  ur = json.loads(requests.get("https://api.deezer.com/track/" + str(a['id'])).text)
               except:
@@ -363,34 +359,25 @@ class Login:
                   array.append(a['name'])
               if len(array) > 1:
                for a in array:
-                   for b in range(len(array)):
-                       try:
-                          if a in array[b] and a != array[b]:
-                           del array[b]
-                       except IndexError:
-                          break
+                   for b in array:
+                       if a in b and a != b:
+                        array.remove(b)
               artist.append(", ".join(OrderedDict.fromkeys(array)))
           album.append(url['title'])
           year.append(url['release_date'])
           try:
              for a in url['genres']['data']:
                  genre.append(a['name'])
-          except:
+          except KeyError:
              None
-          for a in url['contributors']:
-              if a['role'] == "Main":
-               ar_album.append(a['name'])
+          ar_album.append(url['artist']['name'])
           dir = str(output) + "/" + album[0].replace("/", "").replace("$", "S") + "/"
           try:
              os.makedirs(dir)
-          except:
+          except FileExistsError:
              None
-          try:
-             image = requests.get(image).content
-          except:
-             image = requests.get(image).content
           for a in tqdm(range(len(urls))):
-              name = artist[a].replace("/", "").replace("$", "S") + " " + music[a].replace("/", "").replace("$", "S") + ".mp3"
+              name = artist[a].replace("/", "").replace("$", "S") + " " + music[a].replace("/", "").replace("$", "S") + extension
               names.append(dir + name)
               if os.path.isfile(dir + name):
                if check == False:
@@ -400,7 +387,7 @@ class Login:
                if not ans == "y":
                 return
               try:
-                 self.download(urls[a], dir, quality, recursive)
+                 self.download(urls[a], dir, quality, recursive, extension)
               except TrackNotFound:
                  try:
                     url = json.loads(requests.get("https://api.deezer.com/search/track/?q=" + music[a].replace("#", "") + " + " + artist[a].replace("#", "")).text)
@@ -434,10 +421,10 @@ class Login:
                     except IndexError:
                        print("\nTrack not found: " + music[a] + " - " + artist[a])
                        continue
-                 self.download(URL, dir, quality, recursive)
+                 self.download(URL, dir, quality, recursive, extension)
                  urls[a] = URL
               try:
-                 os.rename(dir + urls[a].split("/")[-1] + ".mp3", dir + name)
+                 os.rename(dir + urls[a].split("/")[-1] + extension, dir + name)
               except FileNotFoundError:
                  None
               try:
@@ -472,8 +459,12 @@ class Login:
               except:
                  None
           return names
-      def download_playlistdee(self, URL, output=localdir + "/Songs/", check=True, quality="MP3_128", recursive=True):
+      def download_playlistdee(self, URL, output=localdir + "/Songs/", check=True, quality="MP3_320", recursive=True):
           array = []
+          if quality == "FLAC":
+           extension = ".flac"
+          else:
+              extension = ".mp3"
           if "?utm" in URL:
            URL,a = URL.split("?utm")
           try:
@@ -486,18 +477,20 @@ class Login:
           except KeyError:
              None
           try:
-             if "error" in str(url):
+             if url['error']:
               raise InvalidLink("Invalid link ;)")
           except KeyError:
              None
+          if url['duration'] == 0:
+           raise InvalidLink("Invalid link ;)")
           for a in url['tracks']['data']:
               try:
                  array.append(self.download_trackdee(a['link'], output, check, quality, recursive))
               except TrackNotFound:
                  print("\nTrack not found " + a['title'])
-                 array.append(output + a['title'] + "/" + a['title'] + ".mp3")
+                 array.append(output + a['title'] + "/" + a['title'] + extension)
           return array
-      def download_trackspo(self, URL, output=localdir + "/Songs/", check=True, quality="MP3_128", recursive=True):
+      def download_trackspo(self, URL, output=localdir + "/Songs/", check=True, quality="MP3_320", recursive=True):
           global spo
           if "?" in URL:
            URL,a = URL.split("?")
@@ -524,7 +517,7 @@ class Login:
              return name
           except KeyError:
              raise TrackNotFound("Track not found :(")
-      def download_albumspo(self, URL, output=localdir + "/Songs/", check=True, quality="MP3_128", recursive=True):
+      def download_albumspo(self, URL, output=localdir + "/Songs/", check=True, quality="MP3_320", recursive=True):
           global spo
           if "?" in URL:
            URL,a = URL.split("?")
@@ -553,8 +546,12 @@ class Login:
              return names
           except KeyError:
              raise AlbumNotFound("Album not found :(")
-      def download_playlistspo(self, URL, output=localdir + "/Songs/", check=True, quality="MP3_128", recursive=True):
+      def download_playlistspo(self, URL, output=localdir + "/Songs/", check=True, quality="MP3_320", recursive=True):
           global spo
+          if quality == "FLAC":
+           extension = ".flac"
+          else:
+              extension = ".mp3"
           array = []
           if "?" in URL:
            URL,a = URL.split("?")
@@ -572,7 +569,7 @@ class Login:
                  array.append(self.download_trackspo(a['track']['external_urls']['spotify'], output, check, quality, recursive))
               except TrackNotFound:
                  print("\nTrack not found " + a['track']['name'])
-                 array.append(output + a['track']['name'] + "/" + a['track']['name'] + ".mp3")
+                 array.append(output + a['track']['name'] + "/" + a['track']['name'] + extension)
           if tracks['total'] != 100:
            for a in range(tracks['total'] // 100):
                try:
@@ -586,9 +583,9 @@ class Login:
                       array.append(self.download_trackspo(a['track']['external_urls']['spotify'], output, check, quality, recursive))
                    except TrackNotFound:
                       print("\nTrack not found " + a['track']['name'])
-                      array.append(output + a['track']['name'] + "/" + a['track']['name'] + ".mp3")
+                      array.append(output + a['track']['name'] + "/" + a['track']['name'] + extension)
           return array
-      def download_name(self, artist, song, output=localdir + "/Songs/", check=True, quality="MP3_128", recursive=True):
+      def download_name(self, artist, song, output=localdir + "/Songs/", check=True, quality="MP3_320", recursive=True):
           global spo
           try:
              search = spo.search(q="track:" + song + " artist:" + artist)
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
       name = "deezloader",
-      version = "2.9",
+      version = "3.0.2",
       description = "Downloads songs, albums or playlists from deezer",
       license = "Apache-2.0",
       author = "An0nimia",
