2e55afcbe53350789885db6c06c5f685e5c59c04
An0nimia <41164945+An0nimia@users.noreply.github.com>
2020-01-06 21:35:51 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name = "deezloader",
-	version = "6.0",
+	version = "6.0.1",
 	description = "Downloads songs, albums or playlists from deezer",
 	license = "CC BY-NC-SA 4.0",
 	author = "An0nimia",
